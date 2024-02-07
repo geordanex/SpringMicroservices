ddeb316147f8befe5e00fb5e8ca169dd5d692212
Geordan Sanchez <geordan62@gmail.com>
2024-02-06 19:40:05 -0500

    create BoilerPlate product service

---
+++ product-service/src/main/java/com/gsanchez/productservice/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.gsanchez.productservice.controller;
+
+import com.gsanchez.productservice.dto.ProductRequest;
+import com.gsanchez.productservice.dto.ProductResponse;
+import com.gsanchez.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
