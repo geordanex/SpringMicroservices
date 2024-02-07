4e3c5b4780392096504feb9c749576a0de2a695d
Geordan Sanchez <geordan62@gmail.com>
2024-02-06 22:39:55 -0500

    Refactor MongoContainerClass and adding findAll test using mockMvc and testing repository class

---
+++ product-service/src/test/java/com/gsanchez/productservice/MongoContainerInitialiserTest.java
@@ -0,0 +1,23 @@
+package com.gsanchez.productservice;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class MongoContainerInitialiserTest {
+
+    @Container
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}
--- product-service/src/test/java/com/gsanchez/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/gsanchez/productservice/ProductServiceApplicationTests.java
@@ -2,38 +2,44 @@ package com.gsanchez.productservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gsanchez.productservice.dto.ProductRequest;
+import com.gsanchez.productservice.dto.ProductResponse;
+import com.gsanchez.productservice.service.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Testcontainers
 @AutoConfigureMockMvc
-class ProductServiceApplicationTests {
+class ProductServiceApplicationTests extends MongoContainerInitialiserTest{
 
-	@Container
-	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
 	@Autowired
 	private MockMvc mockMvc;
 	@Autowired
 	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductService productService;
 
-	@DynamicPropertySource
-	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
-		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	@Test
+	void shouldFindAllProducts() throws Exception {
+		productService.createProduct(getProductRequest());
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/product"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Iphone 13")));
 	}
 
 	@Test
@@ -46,6 +52,13 @@ class ProductServiceApplicationTests {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(productRequestString))
 				.andExpect(status().isCreated());
+		assertEquals(productService.getAllProducts().size(), 2);
+	}
+	@Test
+	void shouldFindAllProductsUsingRepository(){
+		productService.createProduct(getProductRequest());
+		List<ProductResponse> results = productService.getAllProducts();
+		assertEquals(results.get(0).getName(), getProductRequest().getName());
 	}
 
 	private ProductRequest getProductRequest() {
